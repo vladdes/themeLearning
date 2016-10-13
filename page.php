5ea2f2a4872ec39cbff94a0cc451b60bdc10c25e
vladdes <vladimir.strukelj@gmail.com>
2016-10-14 00:07:13 +0200

    page styling

--- page.php
+++ page.php
@@ -13,7 +13,7 @@ get_header();
 <?php
 if (have_posts()) :
     while(have_posts()) : the_post();?>
-        <article class="post">
+        <article class="post page">
             <h2><?php the_title();?></h2>
             <?php the_content(); ?>
         </article>
