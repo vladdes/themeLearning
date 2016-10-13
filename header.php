e7452157d0d40275e8d6256d8a6805d96b383c8b
vladdes <vladimir.strukelj@gmail.com>
2016-10-13 17:07:07 +0200

    removed unnecessary files

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
 	<div class="container">
 	<!--site-header-->
 	<header class="site-header">
-		<h1><a href="<?php echo home_url();?>"><?php bloginfo('name')?></a></h1>
+		<h1><?php bloginfo('name')?></h1>
 		<h5><?php bloginfo('description'); ?></h5>
 		<nav class="site-nav">
 			<?php
