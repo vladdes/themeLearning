dd6fc96da72b583ac84c4f6052f8e3ac4820f89d
vladdes <vladimir.strukelj@gmail.com>
2016-10-15 19:41:52 +0200

    page styling

--- special-template.php
+++ special-template.php
@@ -1,9 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: vlad
- * Date: 2016-10-13
- * Time: 16:57
+/*
+ Template Name: Special Layout
  */
 
 
@@ -15,6 +12,12 @@ if (have_posts()) :
     while(have_posts()) : the_post();?>
         <article class="post page">
             <h2><?php the_title();?></h2>
+<!--            info-box-->
+            <div class="info-box">
+                <h4>Some title</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium aliquam aperiam architecto esse,
+                    facilis fuga fugiat inventore labore nesciunt odio perspiciatis quasi qui quibusdam reiciendis ullam unde! Eveniet, odio?</p>
+            </div>
             <?php the_content(); ?>
         </article>
     <?php endwhile;
